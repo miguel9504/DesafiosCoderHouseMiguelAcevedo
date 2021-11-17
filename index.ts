98ce10d75243d813e6d52878f258ebe0d4e33da1
Miguel Acevedo <angel150406@gmail.com>
2021-11-17 18:40:10 -0500

    creacion de un cajero

---
+++ index.ts
@@ -0,0 +1,18 @@
+console.log("Ingresa dinero a retirar: ");
+var stdin = process.openStdin();
+var dinero = 1000000;
+
+stdin.addListener("data", function(d) {
+    retirar(d);
+    stdin.pause();
+});
+
+function retirar (d : any) {
+    var dineroret = d.toString().trim();
+    if (dinero <= dineroret){
+        console.log("No puedes retirar esa cantidad");
+    }else{
+        dinero = dinero - dineroret;
+        console.log("Tu saldo es: " + dinero);
+    }
+}
